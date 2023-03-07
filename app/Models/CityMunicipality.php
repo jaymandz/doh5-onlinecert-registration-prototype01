fa8697f266c14ca7c0250a1e146825401eddac89
Jvergel Mandane <jvergel.mandane@gmail.com>
2023-03-07 09:18:56 +0800

    Added a migration and model for cities and municipalities

---
+++ app/Models/CityMunicipality.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CityMunicipality extends Model
+{
+    use HasFactory;
+
+    protected $table = 'cities_municipalities';
+}
