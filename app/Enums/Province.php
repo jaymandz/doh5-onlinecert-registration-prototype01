2838ec40d3cd259195496efddc3f2bc6f391810f
Jvergel Mandane <jvergel.mandane@gmail.com>
2023-03-07 09:18:39 +0800

    Added a migration and model for provinces

---
+++ app/Enums/Province.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Enums;
+
+enum Province:string {
+    case Albay = 'ALBAY';
+    case CamarinesNorte = 'CAMARINES NORTE';
+    case CamarinesSur = 'CAMARINES SUR';
+    case Catanduanes = 'CATANDUANES';
+    case Masbate = 'MASBATE';
+    case Sorsogon = 'SORSOGON';
+}
