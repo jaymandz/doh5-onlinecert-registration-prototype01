32b1e60e27b2b22ac53f68d215cd5be2dfa29154
unknown <Aldrich Nasayao>
2023-03-08 14:32:47 +0800

    furnished email with information of sender

--- resources/views/emails/notificationMail.blade.php
+++ resources/views/emails/notificationMail.blade.php
@@ -8,8 +8,12 @@
     <p>{{ $details['body'] }}</p>
    
     <p>Thank you!</p><br/>
-    <p><b>ICT Unit</b><br/>
-    <b>Depart of Health - Bicol Center for Health Department</b>
+
+
+    <p><b>Aldrich F. Nasayao<br/>
+    Computer Programmer I<br/>
+    Information and Communications Technology Unit</b><br/>
+    <b>Department of Health - Bicol Center for Health Department</b>
     </p>
     
 </body>
