667cb1dfea98452738dfb50c91f9656b10eb8d65
unknown <Aldrich Nasayao>
2023-03-08 10:55:02 +0800

    adjusted migration 2

--- database/seeders/AssessmentToolsSeeder.php
+++ database/seeders/AssessmentToolsSeeder.php
@@ -16,11 +16,11 @@ class AssessmentToolsSeeder extends Seeder
         $data = ([
             'program_id' => '1', //ABTC
             'facility_id' => '1',
-            'goal_number' => '1',
-            'goal_criterion' => 'The animal Bite Treatment Center / Animal Bite Center provides
-            a safe and effective physical environmetn to its patients and staff',
-            'standard_number' => '1',
-            'standard_criterion' => 'The TB DOTS center is easily located and patients have convenient and safe access to the center.',
+            // 'goal_number' => '1',
+            // 'goal_criterion' => 'The animal Bite Treatment Center / Animal Bite Center provides
+            // a safe and effective physical environmetn to its patients and staff',
+            // 'standard_number' => '1',
+            // 'standard_criterion' => 'The TB DOTS center is easily located and patients have convenient and safe access to the center.',
             'evidence_of_progress' => '',
             'center_rating' => '',   
             'surveyor_comments' => '',   
--- database/seeders/SubgoalsSeeder.php
+++ database/seeders/SubgoalsSeeder.php
@@ -14,13 +14,46 @@ class SubgoalsSeeder extends Seeder
     public function run(): void
     {
         $data = ([
-            'assessment_tool_id' => '1',
-            'standard_number' => '1.1.',
-            'criterion' => 'There is appropriate signage bearing the name of the TB DOTS center to assist patients accessing the center.',
-            'evidence_of_progress' => '',
-            'center_rating' => '',   
-            'surveyor_comments' => '',   
-            'surveyor_team_rating' => ''   
+            [
+                'id' => '1',
+                'assessment_tool_id' => '1',
+                //'standard_number' => '1.1.',
+                //'criterion' => 'There is appropriate signage bearing the name of the TB DOTS center to assist patients accessing the center.',
+                'evidence_of_progress' => '',
+                'center_rating' => '',   
+                'surveyor_comments' => '',   
+                'surveyor_team_rating' => ''
+            ],
+            [
+                'id' => '2',
+                'assessment_tool_id' => '1',
+                //'standard_number' => '1.2.',
+                // 'criterion' => 'There is appropriate signage bearing the name of the TB DOTS center to assist patients accessing the center.',
+                'evidence_of_progress' => '',
+                'center_rating' => '',   
+                'surveyor_comments' => '',   
+                'surveyor_team_rating' => ''
+            ], 
+            [
+                'id' => '3',
+                'assessment_tool_id' => '1',
+                //'standard_number' => '1.2.',
+                // 'criterion' => 'There is appropriate signage bearing the name of the TB DOTS center to assist patients accessing the center.',
+                'evidence_of_progress' => '',
+                'center_rating' => '',   
+                'surveyor_comments' => '',   
+                'surveyor_team_rating' => ''
+            ],
+            [
+                'id' => '3',
+                'assessment_tool_id' => '1',
+                //'standard_number' => '1.2.',
+                // 'criterion' => 'There is appropriate signage bearing the name of the TB DOTS center to assist patients accessing the center.',
+                'evidence_of_progress' => '',
+                'center_rating' => '',   
+                'surveyor_comments' => '',   
+                'surveyor_team_rating' => ''
+            ]   
         ]);
         
         Subgoals::insert($data);
