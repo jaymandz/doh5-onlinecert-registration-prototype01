a86baadbea204869cb70c2f62d47e618000de8cb
Charles Marnie Limpo <charlesmarnielimpo@gmail.com>
2023-03-08 10:52:11 +0800

    Added radio component

--- resources/js/Components/Radio.vue
+++ resources/js/Components/Radio.vue
@@ -26,9 +26,9 @@ const proxyChecked = computed({
 
 <template>
     <input
-        type="checkbox"
+        type="radio"
         :value="value"
         v-model="proxyChecked"
-        class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500"
+        class="w-4 h-4 text-green-600 bg-gray-100 border-gray-300 focus:ring-green-500 dark:focus:ring-green-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
     />
 </template>
